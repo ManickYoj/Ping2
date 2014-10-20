cc1e80c4c031163cf4c062ab1dae3c8d375a4d05
ManickYoj <nickfrancisci@gmail.com>
2014-10-20 13:06:23 -0400

    Added in ping field, corrected input and rendering bugs.

---
+++ pingfield.py
@@ -0,0 +1,71 @@
+from gameobj import *
+from component import *
+from config import *
+from thinkbayes2 import Suite, EvalNormalPdf
+
+
+def newPingField():
+    ping_field = GameObj("ping_field")
+    PingFieldScript(ping_field, "ping", OCEAN_SIZE)
+    return ping_field
+
+
+class PingFieldScript(Renderable):
+
+    def __init__(self, parent, image_path, grid_size):
+        super(PingFieldScript, self).__init__(parent, image_path)
+        self._bayesian = Predictions(grid_size)
+        self._model_changed = True
+
+    def updateModel(self, data):
+        self._model_changed = True
+        self._bayesian.Update(data)
+
+    def render(self, bounds, dt):
+        if self._model_changed:
+            self._model_changed = False
+            self._render_list = []
+            max_prob, items = self._bayesian.getProbData()
+            scaling = SCALE_FACTOR/(2*max_prob)
+
+            for loc, prob in items:
+                size = (int(prob*scaling), int(prob*scaling))
+                img = pygame.transform.smoothscale(self._image, size)
+                if img:
+                    self._render_list.append((img, loc))
+
+        return self._render_list
+
+
+class Predictions(Suite):
+
+    def __init__(self, size):
+        hypos = [(x, y) for x in range(size[0]) for y in range(size[1])]
+        super(Predictions, self).__init__(hypos)
+
+    def Likelihood(self, data, hypo):
+        # Unpack Variables
+        ship_location = hypo
+        shot_location, mean_distance, error = data
+
+        # Calculate Distance from Hypo Point
+        d = distance(shot_location, ship_location)
+
+        # Evaluate Normal Distribution near Ship
+        return EvalNormalPdf(d, mean_distance, error) + .003
+
+    def getProbData(self):
+        max_prob = self.Prob(self.MaximumLikelihood())
+        return max_prob, self.Items()
+
+    def __str__(self):
+        return str(self.getProbData())
+
+
+if __name__ == "__main__":
+    pf = newPingField()
+
+    data1 = ((10, 10), 5, 1)
+    pf.component("PingFieldScript").updateModel(data1)
+
+    print(pf.component("PingFieldScript")._bayesian)
